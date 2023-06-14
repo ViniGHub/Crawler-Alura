0a987a6be2cd611e41459b84aa349b4dbbecbf7d
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2023-06-14 14:07:23 -0300

    Correção do cosigo de buscar-cursos.php

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -12,7 +12,7 @@ $cliente = new Client(['base_uri' => 'https://www.alura.com.br']);
 $crawler = new Crawler();
 
 $buscador = new Buscador($cliente, $crawler);
-$cursos = $buscador->buscar('/cursos-online-programacao/php', 'span.filtro-subcategorias__nome');
+$cursos = $buscador->buscar('/cursos-online-programacao/php', 'span.card-curso__nome');
 
 foreach($cursos as $curso) {
     echo exibeMensagem($curso);
